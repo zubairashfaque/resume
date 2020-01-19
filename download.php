4a444c1becef71296e1fe59c89ad49f813e54e9e
zubair Ashfaque <mianashfaque@gmail.com>
2020-01-19 14:36:39 +0500

    Update download.php

--- download.php
+++ download.php
@@ -2,17 +2,21 @@
 header("Content-Type: application/octet-stream");
 
 $file = $_GET["file"] .".pdf";
-header("Content-Disposition: attachment; filename=" . urlencode($file));   
+$path = $_GET["path"];
+$fullfile = $path.$file;
+
+header("Content-Disposition: attachment; filename=" . Urlencode($file));   
+header("Content-Type: application/force-download");
 header("Content-Type: application/octet-stream");
 header("Content-Type: application/download");
 header("Content-Description: File Transfer");            
-header("Content-Length: " . filesize($file));
+header("Content-Length: " . Filesize($fullfile));
 flush(); // this doesn't really matter.
-$fp = fopen($file, "r");
+$fp = fopen($fullfile, "r");
 while (!feof($fp))
 {
     echo fread($fp, 65536);
     flush(); // this is essential for large downloads
 } 
-fclose($fp); 
+fclose($fp);
 ?>
